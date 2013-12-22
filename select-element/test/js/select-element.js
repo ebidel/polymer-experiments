9352c90cda712727248bb1a76d53ea860549278c
Eric Bidelman <ebidel@gmail.com>
2013-12-21 18:03:17 -0600

    Add select-element

---
+++ select-element/test/js/select-element.js
@@ -0,0 +1,5 @@
+htmlSuite('select-element', function() {
+  htmlTest('html/select-element-basic.html');
+  htmlTest('html/select-element-multiple.html');
+  // htmlTest('html/polymer-selector-multi.html');
+});
