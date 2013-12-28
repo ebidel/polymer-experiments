3ea39e3b55314bd04d34583933b785248e87caa3
Eric Bidelman <ebidel@gmail.com>
2013-12-27 23:30:24 -0500

    More select-element. Just about final

--- select-element/test/js/select-element.js
+++ select-element/test/js/select-element.js
@@ -1,5 +1,6 @@
 htmlSuite('select-element', function() {
   htmlTest('html/select-element-basic.html');
+  htmlTest('html/select-element-add.html');
   htmlTest('html/select-element-multiple.html');
   htmlTest('html/select-element-ui.html');
 });
