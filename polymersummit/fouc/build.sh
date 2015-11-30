c71064acd14d333f0ae5ac2fc358fffbfa8902b1
Eric Bidelman <ebidel@gmail.com>
2015-11-29 16:43:06 -0800

    Perf example updates

--- polymersummit/fouc/build.sh
+++ polymersummit/fouc/build.sh
@@ -11,7 +11,7 @@ js_out_file=elements/elements.v.js
 vulcanize elements/elements.html \
     --inline-script --inline-css --strip-comments | \
     crisper --script-in-head -h $html_out_file -j $js_out_file
-# uglifyjs $js_out_file -c -o $js_out_file
+uglifyjs $js_out_file -c -o $js_out_file
 
 
 # Polymer is="dom=bing" demo ---------------------------------------------------
@@ -19,9 +19,18 @@ vulcanize elements/elements.html \
 html_out_file=elements/elements-no-polymer.v.html
 js_out_file=elements/elements-no-polymer.v.js
 
+vulcanize elements/elements.html \
+    --strip-exclude "bower_components/polymer/" \
+    --inline-script --inline-css --strip-comments | \
+    crisper --script-in-head -h $html_out_file -j $js_out_file
+babel $js_out_file -o $js_out_file
+# uglifyjs $js_out_file -c -o $js_out_file
+
 html_out_file=elements/bootstrap.v.html
 js_out_file=elements/bootstrap.v.js
 
 vulcanize elements/bootstrap.html \
-    --inline-script --inline-css --strip-comments > $html_out_file
+    --inline-script --inline-css --strip-comments | \
+    crisper --script-in-head -h $html_out_file -j $js_out_file
+babel $js_out_file -o $js_out_file
 # uglifyjs $js_out_file -c -o $js_out_file
